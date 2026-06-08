302f61212fd3e0b058af417e73aeb33f0d813c2e
Vedanti Dantwala <68241079+vedanti06@users.noreply.github.com>
2026-06-07 19:45:16 -0700

    Update client.ts
    Signed-off-by: Vedanti Dantwala <68241079+vedanti06@users.noreply.github.com>

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   
- apiKey: "AIzaSyBhlqrh3XPvyQTpMhSoCwlAG-ugyi_okhE",
+ apiKey: "",
  authDomain: "prepwise-6fac5.firebaseapp.com",
  projectId: "prepwise-6fac5",
  storageBucket: "prepwise-6fac5.firebasestorage.app",
- messagingSenderId: "515160831965",
- appId: "1:515160831965:web:b0ff9025817fbc672b2834",
+ messagingSenderId: "",
+ appId: "",
  measurementId: "G-TVTX9F22YV"
 };
 
